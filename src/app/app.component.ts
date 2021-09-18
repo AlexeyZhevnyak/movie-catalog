e877935c7cdad7aeb4f7417855d93cccc1bf657b
leksa911 <leksa680@gmail.com>
2021-09-18 14:41:18 +0300

    route refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component} from '@angular/core';
-import {HeaderSwapService} from "./services/condition/header-swap.service";
-
-import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,16 +6,12 @@ import {Observable} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'movie-catalog';
-  // condition: boolean = true;
-  condition !: Observable<boolean>
 
-  constructor(public condService: HeaderSwapService) {
-    // this.condService.emit(true);
-    // this.condService.condition$.next(true)
+
+  constructor() {
+
 
   }
-
 
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MovieDetailsComponent} from './components/movie-details/movie-details.co
 
 import {MovieCardMenuComponent} from './components/dotted-menu/movie-card-menu.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {HomeComponent} from './components/home/home.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
     FindMovieComponent,
     FilterSortMenuComponent,
     MovieDetailsComponent,
-    MovieCardMenuComponent
+    MovieCardMenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
