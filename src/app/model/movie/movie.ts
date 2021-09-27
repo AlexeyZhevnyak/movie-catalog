d87fced49689a0ab70d020dc449e2f9ae20cc97b
leksa911 <leksa680@gmail.com>
2021-09-27 18:47:39 +0300

    After merging fixes

--- src/app/model/movie/movie.ts
+++ src/app/model/movie/movie.ts
@@ -6,14 +6,14 @@ export class Movie {
   public vote_count: number = 0;
   public release_date: string;
   public poster_path: string = "";
-  public overview: keyof String;
+  public overview: string;
   public budget: number = 0;
   public revenue: number = 0;
   public genres: string[] = []
   public runtime: number = 0;
 
 
-  constructor(id: number, title: string, tagline: string, vote_average: number, vote_count: number, release_date: string, poster_path: string, overview: keyof String, budget: number, revenue: number, genres: string[], runtme: number) {
+  constructor(id: number, title: string, tagline: string, vote_average: number, vote_count: number, release_date: string, poster_path: string, overview: string, budget: number, revenue: number, genres: string[], runtme: number) {
     this.id = id;
     this.title = title;
     this.tagline = tagline;
